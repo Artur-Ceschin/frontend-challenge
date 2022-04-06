7241b428acfe495eb82ad3186621ad0a13592409
Artur-Ceschin <artur.ceschin@gmail.com>
2022-04-05 23:01:05 -0300

    Info cards layout started :tada:

---
+++ starwars-project/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header"
+import { Characters } from "../components/Characters"
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Characters />
+    </>
+  )
+}
